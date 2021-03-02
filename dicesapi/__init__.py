4d416b590fdf466a0b8e7fea760244f8e5f020de
Chris Forstall <cforstall@gmail.com>
2021-03-02 10:06:39 -0400

    add FIEC work

--- dicesapi/__init__.py
+++ dicesapi/__init__.py
@@ -1,107 +1,307 @@
 import requests
 import ipywidgets as widgets
 from IPython.display import display
+from MyCapytain.resolvers.cts.api import HttpCtsResolver
+from MyCapytain.retrievers.cts5 import HttpCtsRetriever
 
 class Author(object):
     '''An ancient author'''
 
-    def __init__(self, data=None):
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)
+        self.id = None
+        self.name = None
+        self.wd = None
+        self.urn = None
+        
         if data:
-            self.data = data
-        else:
-            data = {}
+            self._from_data(data)
 
+
+    def _from_data(self, data):
+        '''populate attributes from data dict'''
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'name' in data:
+            self.name = data['name']
+        if 'wd' in data:
+            self.wd = data['wd']
+        if 'urn' in data:
+            self.urn = data['urn']
+        
 
 class Work(object):
     '''An epic poem'''
     
-    def __init__(self, data=None):
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)
+        self.id = None
+        self.title = None
+        self.wd = None
+        self.urn = None
+        self.author = None
+
         if data:
-            self.data = data
-        else:
-            data = {}
+            self._from_data(data)
+    
+    
+    def _from_data(self, data):
+        '''populate attributes from data dict'''
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'title' in data:
+            self.title = data['title']
+        if 'wd' in data:
+            self.wd = data['wd']
+        if 'urn' in data:
+            self.urn = data['urn']
+        if 'author' in data:
+            if self.index:
+                self.author = self.api.indexedAuthor(data['author'])
+            else:
+                self.author = Author(data['author'], api=self.api)
 
 
 class Character(object):
     '''The base identity of an epic character''' 
     
-    def __init__(self, data=None):
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)        
+        self.id = None
+        self.name = None
+        self.being = None
+        self.type = None
+        self.wd = None
+        self.manto = None
+        
         if data:
-            self.data = data
-        else:
-            data = {}
+            self._from_data(data)
 
+    
+    def _from_data(self, data):
+        '''populate attributes from data'''
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'name' in data:
+            self.name = data['name']
+        if 'being' in data:
+            self.being = data['being']
+        if 'type' in data:
+            self.type = data['type']
+        if 'wd' in data:
+            self.wd = data['wd']
+        if 'manto' in data:
+            self.manto = data['manto']
+
+
+class CharacterInstance(object):
+    '''An instance of a character in context'''
+    
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)        
+        self.id = None
+        self.context = None
+        self.char = None
+        self.disg = None
+        self._name = None
+    
+        if data:
+            self._from_data(data)
+        
+    
+    def _from_data(self, data):
+        '''populate attributes from data'''
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'context' in data:
+            self.context = data['context']
+        if 'char' in data:
+            if self.index:
+                self.char = self.api.indexedCharacter(data['char'])
+            else:
+                self.char = Character(data['char'], api=self.api)
+        if 'disg' in data:
+            # FIXME
+            self.disg = data['disg']
+
+    
+    def getName(self):
+        '''Return a name for the character instance
+        
+           - Defaults to name of underlying character
+        '''
+        
+        if self._name is not None:
+            name = self._name
+        elif self.disg is not None:
+            name = self.char.disg.name
+        else:
+            name = self.char.name
+        
+        return name
+
+
+class SpeechCluster(object):
+    '''A speech cluster'''
+    
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)        
+        self.id = None
+        self.type = None
+        self.work = None
+        
+        if data:
+            self._from_data(data)
+
+
+    def _from_data(self, data):
+        '''populate attributes from data'''
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'type' in data:
+            self.type = data['type']
+        if 'work' in data:
+            if self.index:
+                self.work = self.api.indexedWork(data['work'])
+            else:
+                self.work = Work(data['work'], api=self.api)
+            
 
 class Speech(object):
     '''A single speech'''
 
-    def __init__(self, data=None):
+    def __init__(self, data=None, api=None, index=False):
+        self.api = api
+        self.index = (api is not None and index is not None)        
+        self.data = data
+        self.id = None
+        self.cluster = None
+        self.seq = None
+        self.l_fi = None
+        self.l_la = None
+        self.spkr = None
+        self.addr = None
+        self.part = None
+        
         if data:
-            self.data = data
-        else:
-            data = {}
-    
-    def getURN(self):
-        '''generate urn for the passage'''
-        
-        try:
-            work = self.data['cluster']['work']['urn']
-            l_fi = self.data['l_fi']
-            l_la = self.data['l_la']
-        except KeyError:
-            return None
-        
-        return f'{work}:{l_fi}-{l_la}'
-    
-    def getSpeakers(self):
-        '''Return the list of speakers'''
-        try:
-            spkr = self.data['spkr']
-        except KeyError:
-            return None
-            
-        return [Character(inst['char']) for inst in spkr]
+            self._from_data(data)
 
+        
+    def _from_data(self, data):
+        '''populate attributes from dict'''    
+        
+        if 'id' in data:
+            self.id = data['id']
+        if 'cluster' in data:
+            if self.index:
+                self.cluster = self.api.indexedSpeechCluster(data['cluster'])
+            else:
+                self.cluster = SpeechCluster(data['cluster'], api=self.api)
+        if 'seq' in data:
+            self.seq = data['seq']
+        if 'l_fi' in data:
+            self.l_fi = data['l_fi']
+        if 'l_la' in data:
+            self.l_la = data['l_la']
+        if 'spkr' in data:
+            if self.index:
+                self.spkr = [self.api.indexedCharacterInstance(c)
+                                    for c in data['spkr']]
+            else:
+                self.spkr = [CharacterInstance(c, api=self.api) 
+                                    for c in data['spkr']]
+        if 'addr' in data:
+            if self.index:
+                self.addr = [self.api.indexedCharacterInstance(c)
+                                    for c in data['addr']]
+            else:
+                self.addr = [CharacterInstance(c, api=self.api) 
+                                    for c in data['addr']]
+        if 'part' in data:
+            self.part = data['part']
 
-    def getAddressees(self):
-        '''Return the list of addressees'''
-        try:
-            addr = self.data['addr']
-        except KeyError:
-            return None
-            
-        return [Character(inst['char']) for inst in addr]
-        
-        
-    def getCTS(self, resolver):
+    
+    def __repr__(self):
+        auth = self.cluster.work.author.name
+        work = self.cluster.work.title
+        l_fi = self.l_fi
+        l_la = self.l_la
+        return f'<Speech: {auth} {work} {l_fi}-{l_la}>'
+    
+    
+    @property
+    def work(self):
+        '''shortcut to work (via cluster)'''
+        return self.cluster.work
+    
+    
+    @property
+    def author(self):
+        '''shortcut to work (via cluster)'''
+        return self.work.author
+    
+    
+    @property
+    def l_range(self):
+        '''line range in format <first>-<last>'''
+
+        return f'{self.l_fi}-{self.l_la}'
+    
+
+    @property
+    def urn(self):
+        '''cts urn for the passage'''
+    
+        return f'{self.work.urn}:{self.l_range}'
+    
+    
+    def getCTS(self):
         '''Get the CTS passage corresponding to the speech'''
-        
-        try:
-            work = self.data['cluster']['work']['urn']
-            l_fi = self.data['l_fi']
-            l_la = self.data['l_la']
-        except KeyError:
-            return None
-        
-        cts = resolver.getTextualNode(work, f'{l_fi}-{l_la}')
+                
+        resolver = self.api.resolver
+        cts = resolver.getTextualNode(self.work.urn, self.l_range)
 
         return cts
 
-class SpeechCluster(object):
-    '''A group of related speeches (conversation)'''
-
-    def __init__(self, data=None):
-        if data:
-            self.data = data
+    
+    def getLang(self):
+        '''return language for the speech'''
+        #FIXME
+        
+        if self.author.name in ['Homer', 'Apollonius']:
+            lang = 'greek'
+        elif self.author.name in ['Vergil']:
+            lang = 'latin'
         else:
-            data = {}
+            lang = None
+            
+        return lang
 
 
 class DicesAPI(object):
     '''a connection to the DICES API'''
     
-    def __init__(self, api=None):
-        self.API = api
+    def __init__(self, dices_api=None, cts_api=None):
+        self.API = dices_api
+        self.CTS_API = cts_api
+        self.resolver = HttpCtsResolver(HttpCtsRetriever(self.CTS_API))
+        self._work_index = {}
+        self._author_index = {}
+        self._character_index = {}
+        self._characterinstance_index = {}
+        self._speech_index = {}
+        self._speechcluster_index = {}
+    
 
     def getPagedJSON(self, endpoint, params=None, output=None):
         '''Collect paged results from the API'''
@@ -155,28 +355,50 @@ class DicesAPI(object):
         return results
         
         
-    def getSpeeches(self, **kwargs):
+    def getSpeeches(self, progress=False, **kwargs):
         '''Retrieve speeches from API'''
+        
+        # output
+        if progress:
+            output = widgets.HBox()
+            display(output)
+        else:
+            output = None
+        
+        # get the results from the speeches endpoint
+        results = self.getPagedJSON('speeches', dict(**kwargs), output)
+        
+        # convert to Speech objects
+        speeches = [self.indexedSpeech(s) for s in results]
+        
+        return speeches
+
+
+    def getClusters(self, **kwargs):
+        '''Retrieve speech clusters from API'''
         
         # output
         output = widgets.HBox()
         display(output)
         
         # get the results from the speeches endpoint
-        results = self.getPagedJSON('speeches', dict(**kwargs), output)
+        results = self.getPagedJSON('clusters', dict(**kwargs), output)
         
         # convert to Speech objects
-        speeches = [Speech(s) for s in results]
+        clusters = [self.indexedSpeechCluster(s) for s in results]
         
-        return speeches
-        
+        return clusters
+
     
-    def getCharacters(self, **kwargs):
+    def getCharacters(self, progress=False, **kwargs):
         '''Retrieve speeches from API'''
         
         # output
-        output = widgets.HBox()
-        display(output)
+        if progress:
+            output = widgets.HBox()
+            display(output)
+        else:
+            output = None
         
         # get the results from the speeches endpoint
         results = self.getPagedJSON('characters', dict(**kwargs), output)
@@ -185,3 +407,75 @@ class DicesAPI(object):
         characters = [Character(c) for c in results]
         
         return characters
+    
+
+    def indexedAuthor(self, data):
+        '''Create an author in the index'''
+        
+        if data['id'] in self._author_index:
+            a = self._author_index[data['id']]
+        else:
+            a = Author(data, api=self, index=True)
+            self._author_index[data['id']] = a
+ 
+        return a
+
+
+    def indexedWork(self, data):
+        '''Create a work in the index'''
+        
+        if data['id'] in self._work_index:
+            w = self._work_index[data['id']]
+        else:
+            w = Work(data, api=self, index=True)
+            self._work_index[data['id']] = w
+ 
+        return w
+
+    
+    def indexedSpeech(self, data):
+        '''Create a speech in the index'''
+        
+        if data['id'] in self._speech_index:
+            s = self._speech_index[data['id']]
+        else:
+            s = Speech(data, api=self, index=True)
+            self._speech_index[data['id']] = s
+        
+        return s
+
+        
+    def indexedSpeechCluster(self, data):
+        '''Create a speech cluster in the index'''
+        
+        if data['id'] in self._speechcluster_index:
+            s = self._speechcluster_index[data['id']]
+        else:
+            s = SpeechCluster(data, api=self, index=True)
+            self._speechcluster_index[data['id']] = s
+        
+        return s
+
+
+    def indexedCharacter(self, data):
+        '''Create a character in the index'''
+        
+        if data['id'] in self._character_index:
+            c = self._character_index[data['id']]
+        else:
+            c = Character(data, api=self, index=True)
+            self._character_index[data['id']] = c
+        
+        return c
+
+
+    def indexedCharacterInstance(self, data):
+        '''Create a character instance in the index'''
+        
+        if data['id'] in self._characterinstance_index:
+            c = self._characterinstance_index[data['id']]
+        else:
+            c = CharacterInstance(data, api=self, index=True)
+            self._characterinstance_index[data['id']] = c
+        
+        return c

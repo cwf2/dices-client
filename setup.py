cc05686c5ea7e37e5975a69f91803c4051fa93a9
Chris Forstall <cforstall@gmail.com>
2021-05-10 09:42:08 -0300

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -23,4 +23,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "MyCapytain",
+    ]
 )

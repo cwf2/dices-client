a89ba2e003ce8354705dd22035761875822b1c40
Chris Forstall <cforstall@gmail.com>
2023-05-18 10:43:39 -0300

    work on accommodating both cltk and spacy nlp in text

--- src/dicesapi/text.py
+++ src/dicesapi/text.py
@@ -1,16 +1,18 @@
-'''text - tools for working with CTS passages and CLTK
+'''text - tools for working with CTS passages and NLP
 '''
 
 from MyCapytain.resolvers.cts.api import HttpCtsResolver
 from MyCapytain.retrievers.cts5 import HttpCtsRetriever
 import cltk
+import spacy
 import requests
 from copy import deepcopy
 import re
 
 DEFAULT_SERVERS = {None: 'https://scaife-cts.perseus.org/api/cts'}
-PUNCT = re.compile(r'[ ,·.;\n—]+')
-
+PUNCT = r'[ ,·.;\n—‘’“”]+'
+SPACY_MODEL_GREEK = 'grc_proiel_sm'
+SPACY_MODEL_LATIN = 'la_core_web_sm'
 
 #
 # setup default NLP pipelines
@@ -27,6 +29,19 @@ cltk_nlp['greek'].pipeline.processes = cltk_nlp['greek'].pipeline.processes[:2]
 # trigger installation of stanza data
 cltk_nlp['greek']('χαίρε, ὦ κόσμος')
 cltk_nlp['latin']('salve, mundus')
+
+#
+# spacy setup
+#
+
+spacy_nlp = None
+
+def spacy_load(latin_model=SPACY_MODEL_LATIN, greek_model=SPACY_MODEL_GREEK):
+    global spacy_nlp
+    spacy_nlp = dict(
+        latin = spacy.load(latin_model),
+        greek = spacy.load(greek_model),
+    )
 
 #
 # definitions
@@ -46,11 +61,19 @@ class Passage(object):
         self.speech = speech
         self.line_array = None
         self.cts = None
-        self.cltk = None
+        self.nlp = None
+        self.cltk_doc = None
+        self.spacy_doc = None
         self._line_index = None
         self._token_index = None
+        self._cltk_token_index = None
+        self._spacy_token_index = None
         self.text = None
-    
+
+    @property
+    def cltk(self):
+        '''alias for backward compatibility'''
+        return self.cltk_doc
         
     def buildLineArray(self):
         '''Parse CTS passage into lines'''
@@ -84,34 +107,83 @@ class Passage(object):
         self.text = ' '.join(l['text'] for l in self.line_array)
     
             
-    def runCltkPipeline(self, index=True, remove_punct=False):
+    def runSpacyPipeline(self, index=True, remove=PUNCT):
+        '''Parse text with SpaCy create nlp doc'''
+
+        text = self.text
+
+        if text is None:
+            return
+        if remove is not None:
+            text = re.sub(remove, ' ', self.text).strip()
+        
+        if spacy_nlp is None:
+            spacy_load()
+        
+        self.spacy_doc = spacy_nlp[self.speech.lang](text)
+        self.nlp = self.spacy_doc
+        
+        if index:
+            self.buildSpacyTokenIndex()
+
+    def runCltkPipeline(self, index=True, remove=PUNCT):
         '''Parse text with CLTK pipeline to populate cltk_doc'''
 
         text = self.text
 
         if text is None:
             return
-        if remove_punct:
-            text = PUNCT.sub(' ', self.text).strip()
+        if remove is not None:
+            text = re.sub(remove, ' ', self.text).strip()
 
-        self.cltk = cltk_nlp[self.speech.lang](text)
+        self.cltk_doc = cltk_nlp[self.speech.lang](text)
+        self.nlp = self.cltk_doc
         
         if index:
             self.buildCltkTokenIndex()
+
+
+    def buildTokenIndex(self):
+        '''Alias for backward compatibility'''
+        self.buildCltkTokenIndex()
         
     
     def buildCltkTokenIndex(self):
         '''Create an index linking cltk_doc to line_index'''
         
-        if self.cltk is None:
+        if self.cltk_doc is None:
             return
         
-        self._token_index = []
+        self._cltk_token_index = []
         
         text = self.text
         
-        for w in self.cltk:
+        for w in self.cltk_doc:
             char_pos = text.find(w.string)
+            
+            # if string not found
+            if char_pos == -1:
+                char_pos = None
+            
+            self._cltk_token_index.append(char_pos)
+            length = len(w.string)
+            text = text[:char_pos] + '🧀'*length + text[char_pos+length:]
+        
+        self._token_index = self._cltk_token_index
+
+
+    def buildSpacyTokenIndex(self):
+        '''Create an index linking spacy_doc to line_index'''
+        
+        if self.spacy_doc is None:
+            return
+        
+        self._spacy_token_index = []
+        
+        text = self.text
+        
+        for w in self.spacy_doc:
+            char_pos = text.find(w.text)
             
             # bail if string not found
             if char_pos == -1:
@@ -120,36 +192,60 @@ class Passage(object):
             # otherwise, record position,
             # "cross off" matching string
             
-            self._token_index.append(char_pos)
-            length = len(w.string)
+            self._spacy_token_index.append(char_pos)
+            length = len(w.text)
             text = text[:char_pos] + '🧀'*length + text[char_pos+length:]
+        
+        self._token_index = self._spacy_token_index
     
-    
+
     def getWordIndex(self, word):
         '''Return the word's position in the list of words'''
-        if self.cltk is None:
+        if self.nlp is None:
             return
             
-        return list(self.cltk).index(word)
+        return list(self.nlp).index(word)
+        
+    
+    def getCltkWordIndex(self, word):
+        '''Return the word's position in the list of words'''
+        if self.cltk_doc is None:
+            return
+            
+        return list(self.cltk_doc).index(word)
+
+
+    def getSpacyWordIndex(self, word):
+        '''Return the word's position in the list of words'''
+        if self.spacy_doc is None:
+            return
+            
+        return list(self.spacy_doc).index(word)
 
 
     def getTextPos(self, word):
-        '''Return a word's character position within the string passed to CLTK'''
+        '''Return a word's character position within the string passed to NLP'''
 
-        if self.cltk is None:
+        if self.nlp is None:
             return        
         if self._token_index is None:
             return
         
         # if passed a Word, determine its index
         if isinstance(word, cltk.core.data_types.Word):
-            word = self.getWordIndex(word)
-        
-        return self._token_index[word]
+            word = self.getCltkWordIndex(word)
+            token_index = self._cltk_token_index
+        elif isinstance(word, spacy.tokens.token.Token):
+            word = self.getSpacyWordIndex(word)
+            token_index = self._spacy_token_index
+        else:
+            token_index = self._token_index
+            
+        return token_index[word]
 
 
     def getLineIndex(self, word):
-        if self.cltk is None:
+        if self.nlp is None:
             return
         if self._token_index is None:
             return
@@ -174,6 +270,30 @@ class Passage(object):
         
         return char_pos - self._line_index[i]
 
+
+    def toHTML(self):
+        '''Create a simple html table for passage display'''
+        
+        if self.line_array is not None:
+            html = ''
+            
+            s = self.speech
+            spkr_names = ', '.join([inst.name for inst in s.spkr])
+            addr_names = ', '.join([inst.name for inst in s.addr])
+            
+            html += '<div>'
+            html += f'<h4>{s.author.name} {s.work.title} {s.l_range}</h4>'
+            html += f'<h5>{spkr_names} to {addr_names}</h4>'
+            html += '<table><tbody>'
+    
+            for row in s.passage.line_array:
+                html += f'<tr><td class="n">{row["n"]}</td><td>{row["text"]}</td></tr>'
+    
+            html += '</tbody></table>'
+            html += '</div>'
+    
+            return html
+        
 
 class CtsAPI(object):
     '''interace to digital texts via CTS
